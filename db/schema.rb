1607bd8365a14dff1cafadbed97e0c2c65cbac66
Lizzie <lchan217@gmail.com>
2019-05-08 19:39:30 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,9 @@ ActiveRecord::Schema.define(version: 20190508234250) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "name"
-    t.string   "password"
-    t.string   "password_confirmation"
     t.string   "password_digest"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
