8ed06238d58faad6a2d73b4573de43dfa7b9a589
github username <github email address>
2019-05-10 15:11:06 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,22 @@
 class UsersController < ApplicationController
-    def new
-      @user = User.new
+  def new
+     @user = User.new
+   end
+   def create
+      @user = User.new(user_params) #creates new user
+     if @user.save && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to '/home'
+        #logs you in and sets your password if the confirmation matches
+      else
+        redirect_to '/users/new'
+        #redirects you if your password and confirmation don't match
+
+      end
     end
 
-    def create
-       @user = User.new(user_params)
-      if @user.save && @user.authenticate(params[:user][:password])
-         session[:user_id] = @user.id
-         redirect_to '/home'
-       else
-         redirect_to '/signup'
-       end
-     end
-
-    private
-    def user_params
-      params.require(:user).permit(:username, :name, :password, :password_confirmation)
-    end
-  end
+   private
+   def user_params
+     params.require(:user).permit(:username, :name, :password, :password_confirmation)
+   end
+ end
