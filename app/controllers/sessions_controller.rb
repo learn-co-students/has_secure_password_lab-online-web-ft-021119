7f06667d1d37bdac1a8edb937abede595e517a94
github username <github email address>
2019-05-10 11:35:09 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new
+     @user = User.new
+   end
+   def create #login
+      @user = User.find_by(username: params[:user][:username])
+     if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to '/home'
+      else
+        flash[:alert] = "pw incorrect"
+        redirect_to '/signup'
+      end
+    end
+ end
