51e2849aa33c4b0d6ccde6a4dbbe04cacddc5716
Burak Kocak <burakkocak884@ghotmail.com>
2019-05-10 00:14:44 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/home'
+
+  post 'sessions/create'
+
+  get 'sessions/destroy'
+
+   get 'users/new'
+   post 'users/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
