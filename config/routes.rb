fd46cb3ace69853b129725742486259c4f68dcdd
VBTX <jv.svedaite@gmail.com>
2019-05-10 14:46:15 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create]
+  get '/signup' => 'users#new'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  root 'home#home'
+ 
 end
