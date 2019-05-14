963c2ef46be5a89e26ff80586cb3d972fa7d775e
TaylorSwinton <taylorswinton1@gmail.com>
2019-05-14 15:48:31 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: user_params[:name])
+    if @user.try(:authenticate, user_params[:password])
+      session[:user_id] = @user.id
+       redirect_to user_path(@user)
+    else
+       render :new
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def user_params
+     params.require(:user).permit(:name, :password)
+  end
+
+end
